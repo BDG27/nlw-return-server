0c6ad920ba33eaa4310a3952d885ba088fa3eb17
Bruno Demarchi <brunodemarchi30@gmail.com>
2022-05-08 16:55:34 -0300

    server

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "f095fd6da7bb66",
+      pass: "32c1638a9ea795"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Bruno Demarchi <brunodemarchi30@gmail.com>',
+            subject: subject,
+            html: body
+        });
+    };
+}
